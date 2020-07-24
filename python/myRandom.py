464e2846767d15ac3c0385b7608fc2f560d65768
liuxian2 <liuxian2@lenovo.com>
2020-07-24 11:11:16 +0800

    修改随机数脚本名称

--- python/myRandom.py
+++ python/myRandom.py
@@ -4,12 +4,14 @@ import random
 import string
 
 a=string.ascii_letters + string.digits
-
 key = []
 
 
 def getKey():
     '生成32位随机数'
-    key = random.sample(a, 32)
+    key = random.sample(a, 16)
     keys = "".join(key)
     return keys
+
+
+print("随机数" + getKey())
